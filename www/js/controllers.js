9bf62cd6a5226b2e3f64db73a3a39454513dde4b
BME319 <zjubme319@163.com>
2017-05-15 21:53:16 +0800

    Signed-off-by: BME319 <zjubme319@163.com>

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4098,71 +4098,81 @@ angular.module('kidney.controllers', ['ionic','kidney.services','ngResource','io
         return msgJson;
     }
     function nomoney(){
-        // var alertPopup = $ionicPopup.alert({
-        //      title: '本次咨询已结束',
-        //    });
+        var alertPopup = $ionicPopup.alert({
+             title: '本次咨询已结束',
+           });
 
            // alertPopup.then(function(res) {
            //   console.log('Thank you for not eating my delicious ice cream cone');
            // });
-        var overwarn = $ionicPopup.confirm({
-            title:"您没有提问次数了",
-            template:"是否要再次购买，一次购买增加3次提问",
-            okText:"确认",
-            cancelText:"取消"
-        });
-        overwarn.then(function(confirm){
-            if(confirm){
-                var t = new Date();
-                t = $filter("date")(t, "yyyy-MM-dd HH:mm:ss");
-                var neworder = {
-                    userId:$scope.params.UID,
-                    money:1,
-                    goodsInfo:{
-                      class:'01',
-                      name:$scope.params.counsel.doctorId.name,
-                      notes:$scope.params.chatId
-                    },
-                    paystatus:0,
-                    paytime: t
-                }
-                payment.payment()
-                .then(function(res){
-                    console.log(res);
-                    $ionicLoading.show({
-                        template:'咨询提问购买成功',
-                        duration:1000
-                    });
-                    var msgJson={
-                        contentType:type,
-                        fromID:$scope.params.UID,
-                        fromName:thisPatient.name,
-                        fromUser:{
-                            avatarPath:''
-                        },
-                        targetID:$scope.params.chatId,
-                        targetName:$scope.params.counsel.doctorId.name,
-                        targetType:'single',
-                        status:'send_going',
-                        createTimeInMillis: Date.now(),
-                        newsType:'11',
-                        content:{
-                            type:'counsel-payment'
-                        }
-                    }
-                    socket.emit('message',{msg:msgJson,to:$scope.params.chatId,role:'patient'});
-                    $scope.counselstatus='1';
-                },function(err){
-                    $ionicLoading.show({
-                        template:'购买失败',
-                        duration:2000
-                    });
-                    console.error(err);
-                })
-               //患者知道已经结束了点击了确定
-            }
+        // var overwarn = $ionicPopup.confirm({
+        //     title:"您没有提问次数了",
+        //     template:"是否要再次购买，一次购买增加3次提问",
+        //     okText:"确认",
+        //     cancelText:"取消"
+        // });
+        // overwarn.then(function(confirm){
+        //     if(confirm){
+        //         var t = new Date();
+        //         t = $filter("date")(t, "yyyy-MM-dd HH:mm:ss");
+        //         var neworder = {
+        //             userId:$scope.params.UID,
+        //             money:1,
+        //             goodsInfo:{
+        //               class:'01',
+        //               name:$scope.params.counsel.doctorId.name,
+        //               notes:$scope.params.chatId
+        //             },
+        //             paystatus:0,
+        //             paytime: t
+        //         }
+        //         payment.payment(neworder)
+        //         .then(function(res){
+        //             console.log(res);
+        //             if (res.errMsg == "chooseWXPay:ok")
+        //             {
+        //               $ionicLoading.show({
+        //                   template:'咨询提问购买成功',
+        //                   duration:1000
+        //               });
+        //               var msgJson={
+        //                   contentType:type,
+        //                   fromID:$scope.params.UID,
+        //                   fromName:thisPatient.name,
+        //                   fromUser:{
+        //                       avatarPath:''
+        //                   },
+        //                   targetID:$scope.params.chatId,
+        //                   targetName:$scope.params.counsel.doctorId.name,
+        //                   targetType:'single',
+        //                   status:'send_going',
+        //                   createTimeInMillis: Date.now(),
+        //                   newsType:'11',
+        //                   content:{
+        //                       type:'counsel-payment'
+        //                   }
+        //               }
+        //               socket.emit('message',{msg:msgJson,to:$scope.params.chatId,role:'patient'});
+        //               $scope.counselstatus='1';
+        //             }
+        //             else
+        //             {
+        //               $ionicLoading.show({
+        //                 template:'支付失败，请重新尝试！',
+        //                 duration:2000
+        //             });
+        //             }
+        //         },function(err){
+        //             $ionicLoading.show({
+        //                 template:'购买失败',
+        //                 duration:2000
+        //             });
+        //             console.error(err);
+        //         })
+        //        //患者知道已经结束了点击了确定
+        //     }
 
-        })
+        // })
     }
     function sendmsg(content,type){
         if($scope.counselstatus!=1) return nomoney();
