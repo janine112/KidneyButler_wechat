7b1d9377e9aebd7b2a4c55abe3a6aae13a767a7d
BME319 <zjubme319@163.com>
2017-05-22 23:31:09 +0800

    Signed-off-by: BME319 <zjubme319@163.com>

--- www/js/app.js
+++ www/js/app.js
@@ -96,7 +96,7 @@ angular.module('kidney',['ionic','kidney.services','kidney.controllers','kidney.
                     // $scope.logStatus = "登录成功！";
                     $ionicHistory.clearCache();
                     $ionicHistory.clearHistory();
-                    User.getUserIDbyOpenId({openId:wechatData.openid}).then(function(data)
+                    User.getUserIDbyOpenId({openId:Storage.get('openid')}).then(function(data)
                     {
                         if (angular.isDefined(data.phoneNo) == true)
                         {
@@ -105,7 +105,7 @@ angular.module('kidney',['ionic','kidney.services','kidney.controllers','kidney.
                     },function(err)
                     {
                         console.log(err)
-                    })
+                    })  
                     Storage.set('TOKEN',data.results.token);//token作用目前还不明确
                     Storage.set('isSignIn',"Yes");
                     Storage.set('UID',data.results.userId);
