1d27cc229d37992ba6ef8d9ab35df890aa558210
X-WORK\Xiongjzh <xiongjzh@gmail.com>
2017-08-04 19:15:13 +0800

    timeout

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4016,11 +4016,14 @@ angular.module('kidney.controllers', ['ionic','kidney.services','ngResource','io
         msg.direct = msg.fromID==$scope.params.UID?'send':'receive';
         if(msg.contentType=='image') {
             msg.content.thumb=CONFIG.mediaUrl+msg.content['src_thumb'];
-            $http.get(msg.content.thumb).then(function(data){
+            $timeout(function(){
+              $http.get(msg.content.thumb).then(function(data){
                 $scope.msgs.push(msg);
                 toBottom(true,600);
                 $scope.params.msgCount++;
-            })
+              })
+            },1000)
+            
         }else{
             $scope.msgs.push(msg);
             toBottom(true,200);
